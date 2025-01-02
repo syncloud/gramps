f26bcbe3f31dec5a05e3ea20124a253a83cd30fb
Boris Rybalkin <ribalkin@gmail.com>
2025-01-02 17:49:26 +0000

    config fix

--- bin/configure.sh
+++ bin/configure.sh
@@ -2,4 +2,7 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 cd $DIR/gramps/app/src
-$DIR/gramps/sbin/python3 -m gramps_webapi --config /var/snap/gramps/current/config/gramps.cfg user migrate
+$DIR/gramps/sbin/python \
+  -m gramps_webapi \
+  --config /var/snap/gramps/current/config/gramps.cfg \
+  user migrate
--- bin/service.gunicorn.sh
+++ bin/service.gunicorn.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-#export HOME=$DIR/gramps/app
-#cd $HOME/src
 $DIR/bin/wait-for-configure.sh
 if [[ -f /var/snap/platform/current/CI_TEST ]]; then
   export REQUESTS_CA_BUNDLE=/var/snap/platform/current/syncloud.ca.crt
 fi
+export GRAMPS_API_CONFIG=/var/snap/gramps/current/config/gramps.cfg
 exec $DIR/gramps/sbin/python \
   ${DIR}/gramps/usr/local/bin/gunicorn \
   -w 8 \
