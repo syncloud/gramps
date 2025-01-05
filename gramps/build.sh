55f73dc8870cdbb1d5ff4151ca720710d547d8da
Boris Rybalkin <ribalkin@gmail.com>
2025-01-05 22:30:10 +0000

    no first screen 2

--- gramps/build.sh
+++ gramps/build.sh
@@ -6,8 +6,8 @@ BUILD_DIR=${DIR}/../build/snap/gramps
 mkdir -p ${BUILD_DIR}
 
 sed -i 's#sys\.executable#"/snap/gramps/current/gramps/sbin/python"#g' /app/src/gramps_webapi/__main__.py
-sed -i 's#if get_all_user_details(#if current_app.config["LDAP_ENABLED"] or get_all_user_details(#g' /app/src/gramps_webapi/api/resources/token.py
-
+#sed -i 's#if get_all_user_details(#if current_app.config["LDAP_ENABLED"] or get_all_user_details(#g' /app/src/gramps_webapi/api/resources/token.py
+sed -i 's#if get_all_user_details(#if True or get_all_user_details(#g' /app/src/gramps_webapi/api/resources/token.py
 cp -r /bin ${BUILD_DIR}
 cp -r /usr ${BUILD_DIR}
 cp -r /lib ${BUILD_DIR}
