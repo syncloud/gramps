fef136a80c51c11e8f5a3322ecadbd8414e7bef5
Boris Rybalkin <ribalkin@gmail.com>
2025-01-05 12:25:39 +0000

    fix python

--- gramps/build.sh
+++ gramps/build.sh
@@ -4,6 +4,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 BUILD_DIR=${DIR}/../build/snap/gramps
 mkdir -p ${BUILD_DIR}
+sed -i 's#sys\.executable#"/snap/gramps/current/gramps/sbin/python"#g' /app/src/gramps_webapi/__main__.py
 cp -r /bin ${BUILD_DIR}
 cp -r /usr ${BUILD_DIR}
 cp -r /lib ${BUILD_DIR}
