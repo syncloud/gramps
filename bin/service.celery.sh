9afd53005e616ce227f3519b84a0bc75cbc4785f
Boris Rybalkin <ribalkin@gmail.com>
2025-01-05 15:41:03 +0000

    static

--- bin/service.celery.sh
+++ bin/service.celery.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-export HOME=$DIR/gramps/usr/src/gramps
-cd $HOME/src
-export PATH=$DIR/gramps/sbin:$PATH
 $DIR/bin/wait-for-configure.sh
-exec $DIR/gramps/sbin/python ${DIR}/gramps/usr/local/bin/celery -A gramps_webapi.celery worker --loglevel=INFO --concurrency=2
+exec $DIR/gramps/sbin/python \
+  ${DIR}/gramps/usr/local/bin/celery \
+  -A gramps_webapi.celery \
+  worker \
+  --loglevel=INFO \
+  --concurrency=2
