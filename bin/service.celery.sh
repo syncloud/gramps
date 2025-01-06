aba00644b2e04fe6e108f28c2d5f840a12881180
Boris Rybalkin <ribalkin@gmail.com>
2025-01-06 18:24:14 +0000

    celery config

--- bin/service.celery.sh
+++ bin/service.celery.sh
@@ -2,6 +2,7 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 $DIR/bin/wait-for-configure.sh
+export GRAMPS_API_CONFIG=/var/snap/gramps/current/config/gramps.cfg
 exec $DIR/gramps/sbin/python \
   ${DIR}/gramps/usr/local/bin/celery \
   -A gramps_webapi.celery \
