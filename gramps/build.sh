2a9303785a2390f2b8f0cc99ef229b6244d7e552
Boris Rybalkin <ribalkin@gmail.com>
2025-01-07 09:19:16 +0000

    sqlite

--- gramps/build.sh
+++ gramps/build.sh
@@ -8,6 +8,8 @@ mkdir -p ${BUILD_DIR}
 sed -i 's#sys\.executable#"/snap/gramps/current/gramps/sbin/python"#g' /app/src/gramps_webapi/__main__.py
 #sed -i 's#if get_all_user_details(#if current_app.config["LDAP_ENABLED"] or get_all_user_details(#g' /app/src/gramps_webapi/api/resources/token.py
 sed -i 's#if get_all_user_details(#if True or get_all_user_details(#g' /app/src/gramps_webapi/api/resources/token.py
+sed -i 's#dbid = "bsddb"#dbid = "sqlite"#g' /usr/local/lib/python3.11/dist-packages/gramps/gen/db/utils.py
+
 cp -r /bin ${BUILD_DIR}
 cp -r /usr ${BUILD_DIR}
 cp -r /lib ${BUILD_DIR}
