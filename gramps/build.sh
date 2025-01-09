39e808e71cf611e9d5e3b4786437c6fed05a8c20
Boris Rybalkin <ribalkin@gmail.com>
2025-01-09 20:48:18 +0000

    login

--- gramps/build.sh
+++ gramps/build.sh
@@ -5,8 +5,17 @@ cd ${DIR}
 BUILD_DIR=${DIR}/../build/snap/gramps
 mkdir -p ${BUILD_DIR}
 
+pip install python-ldap==3.4.0
+
+mkdir tmp
+cd tmp
+wget -c --progress=dot:giga https://github.com/cyberb/gramps-web-api/archive/refs/heads/master.zip
+unzip master.zip
+cp gramps-web-api-master/gramps_webapi/__init__.py /usr/local/lib/python3.11/dist-packages/gramps_webapi/auth
+cd ..
+rm -rf tmp
+
 sed -i 's#sys\.executable#"/snap/gramps/current/gramps/sbin/python"#g' /app/src/gramps_webapi/__main__.py
-#sed -i 's#if get_all_user_details(#if current_app.config["LDAP_ENABLED"] or get_all_user_details(#g' /app/src/gramps_webapi/api/resources/token.py
 sed -i 's#if get_all_user_details(#if True or get_all_user_details(#g' /usr/local/lib/python3.11/dist-packages/gramps_webapi/api/resources/token.py
 sed -i 's#dbid = "bsddb"#dbid = "sqlite"#g' /usr/local/lib/python3.11/dist-packages/gramps/gen/db/utils.py
 
--- test/ui.py
+++ test/ui.py
@@ -1,10 +1,7 @@
-import time
-from os.path import dirname, join
-from subprocess import check_output
-
 import pytest
+from os.path import dirname, join
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from subprocess import check_output
 from syncloudlib.integration.hosts import add_host_alias
 
 DIR = dirname(__file__)
@@ -32,22 +29,10 @@ def test_start(module_setup, app, domain, device_host):
 
 def test_login(selenium, device_user, device_password):
     selenium.open_app()
-    #selenium.find_by(By.XPATH, "//button[contains(.,'Syncloud')]").click()
-    #selenium.find_by(By.XPATH, "//a[contains(.,'My Syncloud')]").click()
-    selenium.find_by(By.ID, "username-textfield").send_keys(device_user)
-    password = selenium.find_by(By.ID, "password-textfield")
+    selenium.find_by(By.ID, "username").send_keys(device_user)
+    password = selenium.find_by(By.ID, "password")
     password.send_keys(device_password)
     selenium.screenshot('login')
-    #password.send_keys(Keys.RETURN)
-    selenium.find_by(By.ID, "sign-in-button").click()
-    selenium.find_by(By.ID, "accept-button").click()
-    selenium.find_by(By.XPATH, "//button[contains(.,'Sign up')]").click()
+    selenium.find_by(By.XPATH, "//button[@type='submit']").click()
     selenium.find_by(By.XPATH, "//h4[contains(.,'Gtamps  is running!')]")
     selenium.screenshot('main')
-
-def test_upload(selenium):
-    file = selenium.find_by(By.XPATH, "//input[@type='file']")
-    selenium.driver.execute_script("arguments[0].removeAttribute('class')", file)
-    file.send_keys(join(DIR, '..', 'gramps', 'simple.pdf'))
-    selenium.find_by(By.XPATH, "//span[contains(.,'Dismiss completed')]")
-    selenium.screenshot('uploaded')
