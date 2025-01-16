c83cf7c6b94f4fa2c039e40745a5658b212a57c3
Boris Rybalkin <ribalkin@gmail.com>
2025-01-16 15:09:13 +0000

    fix fonts

--- bin/configure.sh
+++ bin/configure.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+. "${SNAP_DATA}/config/env"
 cd $DIR/gramps/app/src
 $DIR/gramps/sbin/python \
   -m gramps_webapi \
--- gramps/build.sh
+++ gramps/build.sh
@@ -20,7 +20,8 @@ rm -rf tmp
 sed -i 's#sys\.executable#"/snap/gramps/current/gramps/sbin/python"#g' /app/src/gramps_webapi/__main__.py
 sed -i 's#if get_all_user_details(#if True or get_all_user_details(#g' /usr/local/lib/python3.11/dist-packages/gramps_webapi/api/resources/token.py
 #sed -i 's#dbid = "bsddb"#dbid = "sqlite"#g' /usr/local/lib/python3.11/dist-packages/gramps/gen/db/utils.py
-sed -i 's#dbid_path = os.path.join(dirpath, DBBACKEND)#dbid_path = os.path.join(dirpath, DBBACKEND); print(dbid_path)#g' /usr/local/lib/python3.11/dist-packages/gramps/gen/db/utils.py
+#sed -i 's#dbid_path = os.path.join(dirpath, DBBACKEND)#dbid_path = os.path.join(dirpath, DBBACKEND); print(dbid_path)#g' /usr/local/lib/python3.11/dist-packages/gramps/gen/db/utils.py
+sed -i 's#<dir>/usr/share/fonts</dir>#<dir>/snap/gramps/current/gramps/usr/share/fonts</dir>#g' /etc/fonts/fonts.conf
 
 cp -r /bin ${BUILD_DIR}
 cp -r /usr ${BUILD_DIR}
