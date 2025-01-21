854a685bf14d841c25549cec0e45820c9729f982
Boris Rybalkin <ribalkin@gmail.com>
2025-01-21 18:58:22 +0000

    fix

--- test/ui.py
+++ test/ui.py
@@ -131,7 +131,7 @@ def test_backup(selenium, device, artifact_dir, device_host, device_user, device
     login(device_password, device_user, selenium)
     assert stats(selenium).text == "0"
 
-    device.run_ssh('ls -la /var/snap/gramps/current/gramps/grampsdb'), throw=False)
+    device.run_ssh('ls -la /var/snap/gramps/current/gramps/grampsdb', throw=False)
         
     device.run_ssh("snap run platform.cli backup restore {0}".format(backup['file']))
     wait_for_rest(requests.session(), "https://{0}".format(app_domain), 200, 10)
@@ -175,3 +175,4 @@ def stats(selenium):
         '.querySelector("table > tr:nth-child(1) > td")'
     )
     return name
+
