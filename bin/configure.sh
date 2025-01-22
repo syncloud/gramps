242785f5d2ed29070e651a9a2ca327603da1fca2
Boris Rybalkin <ribalkin@gmail.com>
2025-01-22 17:25:16 +0000

    no configure

--- bin/configure.sh
+++ bin/configure.sh
@@ -3,7 +3,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 . "${SNAP_DATA}/config/env"
 cd $DIR/gramps/app/src
-$DIR/gramps/sbin/python \
-  -m gramps_webapi \
-  --config /var/snap/gramps/current/config/gramps.cfg \
-  user migrate
+#$DIR/gramps/sbin/python \
+#  -m gramps_webapi \
+#  --config /var/snap/gramps/current/config/gramps.cfg \
+#  user migrate
