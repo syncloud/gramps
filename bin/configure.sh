567ada3b7dab8251c1bccf55e7d411eb7b278796
Boris Rybalkin <ribalkin@gmail.com>
2025-01-22 20:16:03 +0000

    configure logs

--- bin/configure.sh
+++ bin/configure.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+ls -1 /var/snap/gramps/current/gramps/grampsdb | logger -t gramps
 . "${SNAP_DATA}/config/env"
 cd $DIR/gramps/app/src
 $DIR/gramps/sbin/python \
   -m gramps_webapi \
   --config /var/snap/gramps/current/config/gramps.cfg \
   user migrate
+ls -1 /var/snap/gramps/current/gramps/grampsdb | logger -t gramps
