e19a90fdfd8d233ac05ccb3a3fb31a311d1653ff
Boris Rybalkin <ribalkin@gmail.com>
2025-01-22 21:37:26 +0000

    more logs

--- bin/service.celery.sh
+++ bin/service.celery.sh
@@ -3,6 +3,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 $DIR/bin/wait-for-configure.sh
 . "${SNAP_DATA}/config/env"
+ls -1 /var/snap/gramps/current/gramps/grampsdb
 exec $DIR/gramps/sbin/python \
   ${DIR}/gramps/usr/local/bin/celery \
   -A gramps_webapi.celery \
