da32e916b02300c73fb67b65dd5dbbeddef79684
Boris Rybalkin <ribalkin@gmail.com>
2025-01-26 20:17:25 +0000

    fix backup

--- test/ui.py
+++ test/ui.py
@@ -19,7 +19,7 @@ def module_setup(request, device, artifact_dir, ui_mode, driver, selenium):
         device.activated()
         device.run_ssh('mkdir -p {0}'.format(TMP_DIR), throw=False)
         device.run_ssh('journalctl > {0}/journalctl.ui.log'.format(TMP_DIR, ui_mode), throw=False)
-        device.run_ssh('ls -la /var/snap/gramps/current/grampsdb > {0}/grampsdb.ui.log'.format(TMP_DIR), throw=False)
+        device.run_ssh('ls -la /var/snap/gramps/current/gramps/grampsdb > {0}/grampsdb.ui.log'.format(TMP_DIR), throw=False)
         device.run_ssh(
             'cat /var/snap/platform/current/config/authelia/config.yml > {0}/authelia.config.ui.log'.format(TMP_DIR),
             throw=False)
@@ -38,25 +38,7 @@ def test_start(module_setup, device, app, domain, device_host):
 
 def test_login(selenium, device_user, device_password):
     selenium.open_app()
-    selenium.find_by(By.ID, "username").send_keys(device_user)
-    password = selenium.find_by(By.ID, "password")
-    password.send_keys(device_password)
-    selenium.screenshot('login')
-
-    selenium.element_by_js(
-        'document'
-        '.querySelector("gramps-js").shadowRoot'
-        '.querySelector("grampsjs-login").shadowRoot'
-        '.querySelector("mwc-button")'
-    ).click()
-
-    home = selenium.element_by_js(
-        'document'
-        '.querySelector("gramps-js").shadowRoot'
-        '.querySelector("grampsjs-main-menu").shadowRoot'
-        '.querySelector("mwc-list grampsjs-list-item span")'
-    )
-    assert home.text == "Home Page"
+    login(device_password, device_user, selenium)
     selenium.screenshot('main')
 
 
@@ -135,19 +117,66 @@ def test_add_person(selenium):
     selenium.screenshot('add-person-saved')
 
 
-def test_backup(selenium, device, artifact_dir, device_host, device_password, app_archive_path, app_domain):
+def test_backup(selenium, device, artifact_dir, device_host, device_user, device_password, app_archive_path, app_domain):
+
+    device.run_ssh("cat /var/snap/gramps/current/gramps/recent-files-gramps.xml", throw=False)
+    databases = device.run_ssh("ls -1 /var/snap/gramps/current/gramps/grampsdb", throw=False).splitlines()
+    assert len(databases) == 1
+
     device.run_ssh("snap run platform.cli backup create gramps")
     response = device.run_ssh("snap run platform.cli backup list")
     open('{0}/cli.backup.list.json'.format(artifact_dir), 'w').write(response)
-    print(response)
     backup = json.loads(response)[0]
     device.run_ssh('tar tvf {0}/{1}'.format(backup['path'], backup['file']))
     device.run_ssh("snap remove gramps")
     local_install(device_host, device_password, app_archive_path)
     wait_for_rest(requests.session(), "https://{0}".format(app_domain), 200, 10)
-    device.run_ssh("snap run platform.cli backup restore {0}".format(backup['file']))
-    wait_for_rest(requests.session(), "https://{0}".format(app_domain), 200, 10)
+
     selenium.open_app()
+    login(device_password, device_user, selenium)
+    assert stats(selenium).text == "0"
+
+    databases = device.run_ssh('ls -1 /var/snap/gramps/current/gramps/grampsdb', throw=False).splitlines()
+    assert len(databases) == 1
+    
+    device.run_ssh("snap run platform.cli backup restore {0}".format(backup['file']))
+    device.run_ssh("cat /var/snap/gramps/current/gramps/recent-files-gramps.xml", throw=False)
+    databases = device.run_ssh('ls -1 /var/snap/gramps/current/gramps/grampsdb', throw=False).splitlines()
+    assert len(databases) == 1
+
+    wait_for_rest(requests.session(), "https://{0}".format(app_domain), 200, 10)
+
+    databases = device.run_ssh('ls -1 /var/snap/gramps/current/gramps/grampsdb', throw=False).splitlines()
+    assert len(databases) == 1
+
+    selenium.open_app()
+    login(device_password, device_user, selenium)
+    assert stats(selenium).text == "1"
+
+    selenium.screenshot('backup-restored')
+
+
+def login(device_password, device_user, selenium):
+    selenium.find_by(By.ID, "username").send_keys(device_user)
+    password = selenium.find_by(By.ID, "password")
+    password.send_keys(device_password)
+    selenium.screenshot('login')
+    selenium.element_by_js(
+        'document'
+        '.querySelector("gramps-js").shadowRoot'
+        '.querySelector("grampsjs-login").shadowRoot'
+        '.querySelector("mwc-button")'
+    ).click()
+    home = selenium.element_by_js(
+        'document'
+        '.querySelector("gramps-js").shadowRoot'
+        '.querySelector("grampsjs-main-menu").shadowRoot'
+        '.querySelector("mwc-list grampsjs-list-item span")'
+    )
+    assert home.text == "Home Page"
+
+
+def stats(selenium):
     name = selenium.element_by_js(
         'document'
         '.querySelector("body > gramps-js").shadowRoot'
@@ -156,6 +185,4 @@ def test_backup(selenium, device, artifact_dir, device_host, device_password, ap
         '.querySelector("#statistics").shadowRoot'
         '.querySelector("table > tr:nth-child(1) > td")'
     )
-    assert name.text == "1"
-
-    selenium.screenshot('backup-restored')
+    return name
